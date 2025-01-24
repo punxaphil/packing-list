25e7c1233cfacf368896f8baf9b27aeb94fce122
Johan Frick <johan.frick@gmail.com>
2025-01-24 14:23:50 +0100

    vercel.json

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import Redis from 'ioredis';
+import path from 'node:path';
 
 const redis = new Redis();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Serve the static files from the React app
+const clientDir = path.join(__dirname, '../../client/dist');
 
 app.get('/read', async (req, res) => {
   try {
@@ -29,6 +33,11 @@ app.post('/write', async (req, res) => {
   }
 });
 
+app.use(express.static(clientDir));
+// Handle requests by serving index.html for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientDir, 'index.html'));
+});
 app.listen(8080, () => {
   console.log('server listening on port 8080');
 });
