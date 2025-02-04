b7407b40c7e1308bdeb9da17371073bfbdb8044f
Frida Frick <frida@kodsam.se>
2025-02-04 23:28:39 +0100

    Initial filter category

--- src/components/pages/PackingList.tsx
+++ src/components/pages/PackingList.tsx
@@ -1,60 +1,82 @@
 import { useState } from 'react';
 import { PackItem } from '../../types/PackItem.ts';
 import { useFirebase } from '../../services/contexts.ts';
-import { Box, Card, CardBody, Flex, Text, Image } from '@chakra-ui/react';
+import { Box, Card, CardBody, Flex, Text, Image, Stack, Heading } from '@chakra-ui/react';
 import { AddOrEditPackItem } from '../packing-list/AddOrEditPackItem.tsx';
 import { groupByCategories } from '../../services/utils.ts';
 import { PackItemRow } from '../packing-list/PackItemRow.tsx';
+import { PLSelect } from '../shared/PLSelect.tsx';
 
 export function PackingList() {
   const [selectedItem, setSelectedItem] = useState<PackItem>();
-
-  const images = useFirebase().images;
   const packItems = useFirebase().packItems;
+  const [filteredPackItems, setFilteredPackItems] = useState(packItems);
+  const images = useFirebase().images;
+
   const categories = useFirebase().categories;
-  const grouped = groupByCategories(packItems);
+  const grouped = groupByCategories(filteredPackItems);
 
   function getCategoryImage(typeId: string) {
     const image = images.find((t) => t.type === 'categories' && t.typeId === typeId);
     return image?.url;
   }
 
+  function filterOnCategory(category: string) {
+    setFilteredPackItems(packItems.filter((packItem) => packItem.category === category));
+  }
+
   return (
     <Box mt="5" maxWidth="600px" mx="auto">
-      {packItems.length ? (
-        <Card>
-          <CardBody>
-            {Object.entries(grouped).map(([groupCategory, packItems]) => (
-              <Box key={groupCategory}>
-                {groupCategory && (
-                  <Flex gap="3" alignItems="center" mt="5">
-                    {getCategoryImage(groupCategory) && (
-                      <Image borderRadius="full" boxSize="30px" src={getCategoryImage(groupCategory)}></Image>
-                    )}
+      {filteredPackItems.length ? (
+        <>
+          {/* Move to a seperate component */}
+          <Flex mb="3" gap="2" direction="column">
+            <Heading as="h2" size="md">
+              Filters
+            </Heading>
+            <Stack direction="row" justifyContent="space-between" alignItems="center">
+              <PLSelect
+                setSelection={filterOnCategory}
+                selected=""
+                placeholder="Select category"
+                options={categories}
+              />
+            </Stack>
+          </Flex>
+          {/* ---- */}
+          <Card>
+            <CardBody>
+              {Object.entries(grouped).map(([groupCategory, packItems]) => (
+                <Box key={groupCategory}>
+                  {groupCategory && (
+                    <Flex gap="3" alignItems="center" mt="5">
+                      {getCategoryImage(groupCategory) && (
+                        <Image borderRadius="full" boxSize="30px" src={getCategoryImage(groupCategory)}></Image>
+                      )}
 
-                    <Box>
-                      <Text as="b">{categories.find((cat) => cat.id === groupCategory)?.name ?? ''}</Text>
-                    </Box>
-                  </Flex>
-                )}
-                {packItems.map((packItem) => (
-                  <PackItemRow
-                    packItem={packItem}
-                    key={packItem.id}
-                    onEdit={setSelectedItem}
-                    indent={!!groupCategory}
-                  />
-                ))}
-              </Box>
-            ))}
-          </CardBody>
-        </Card>
+                      <Box>
+                        <Text as="b">{categories.find((cat) => cat.id === groupCategory)?.name ?? ''}</Text>
+                      </Box>
+                    </Flex>
+                  )}
+                  {packItems.map((packItem) => (
+                    <PackItemRow
+                      packItem={packItem}
+                      key={packItem.id}
+                      onEdit={setSelectedItem}
+                      indent={!!groupCategory}
+                    />
+                  ))}
+                </Box>
+              ))}
+            </CardBody>
+          </Card>
+        </>
       ) : (
         <Flex justifyContent="center" minWidth="max-content">
           <Text>No items yet.</Text>
         </Flex>
       )}
-      {/* </Flex> */}
       <Card mt="5">
         <CardBody>
           <AddOrEditPackItem
