7f45fa930e35e4a6e966453080ab762358fe4610
Johan Frick <johan.frick@gmail.com>
2025-02-11 22:30:11 +0100

    fix: center content Welcome.tsx

--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -1,15 +1,15 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Stack } from '@chakra-ui/react';
 import { Login } from '../auth/Auth.tsx';
 
 export function Welcome({ title }: { title: string }) {
   return (
-    <>
+    <Stack alignItems="center">
       <Heading as="h3" m="6">
         Welcome to {title}
       </Heading>
-      <Box mb="2">Start preparing your trip by logging in or registering below ✈️</Box>
+      <Box mb="2">Start preparing your trip by logging in or registering below ✈</Box>
       <img src="/squirrel_400.png" alt="squirrel" style={{ maxWidth: '90%' }} />
       <Login />
-    </>
+    </Stack>
   );
 }
