f40f793a9b5fce6b1811d3414bdaaf58fcb39648
Johan Frick <johan.frick@gmail.com>
2025-02-24 19:10:10 +0100

    feat: show tooltip for long pack item names

--- src/components/shared/PLInput.tsx
+++ src/components/shared/PLInput.tsx
@@ -1,5 +1,5 @@
-import { Input } from '@chakra-ui/react';
-import { ChangeEvent, KeyboardEvent, useState } from 'react';
+import { Input, Tooltip } from '@chakra-ui/react';
+import { ChangeEvent, KeyboardEvent, useCallback, useEffect, useRef, useState } from 'react';
 import { handleEnter } from '../../services/utils.ts';
 
 export function PLInput({
@@ -18,6 +18,8 @@ export function PLInput({
   onFocus?: () => void;
 }) {
   const [text, setText] = useState(value);
+  const [overflowActive, setOverflowActive] = useState(false);
+  const textRef = useRef<HTMLInputElement>(null);
 
   async function onChange(e: ChangeEvent<HTMLInputElement>) {
     setText(e.target.value);
@@ -29,20 +31,37 @@ export function PLInput({
       onEnter?.();
     });
   }
+
+  const isOverflowActive = useCallback((event: HTMLInputElement) => {
+    return event.offsetHeight < event.scrollHeight || event.offsetWidth < event.scrollWidth;
+  }, []);
+
+  useEffect(() => {
+    if (textRef?.current && isOverflowActive(textRef.current)) {
+      setOverflowActive(true);
+      return;
+    }
+
+    setOverflowActive(false);
+  }, [isOverflowActive]);
+
   return (
-    <Input
-      fontWeight={bold ? 'bold' : 'normal'}
-      overflow="hidden"
-      textDecoration={strike ? 'line-through' : 'none'}
-      textOverflow="ellipsis"
-      whiteSpace="nowrap"
-      value={text}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-      onBlur={() => onUpdate(text)}
-      variant="unstyled"
-      height="32px"
-      onFocus={onFocus}
-    />
+    <Tooltip label={overflowActive && text}>
+      <Input
+        fontWeight={bold ? 'bold' : 'normal'}
+        overflow="hidden"
+        textDecoration={strike ? 'line-through' : 'none'}
+        textOverflow="ellipsis"
+        whiteSpace="nowrap"
+        value={text}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        onBlur={() => onUpdate(text)}
+        variant="unstyled"
+        height="32px"
+        onFocus={onFocus}
+        ref={textRef}
+      />
+    </Tooltip>
   );
 }
