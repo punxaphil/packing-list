4ffa61fbc4afad986096fe21401b7c7cf978617a
Johan Frick <johan.frick@gmail.com>
2025-03-14 22:12:56 +0100

    fix: too small margin contextmenu button

--- src/components/shared/ContextMenu.tsx
+++ src/components/shared/ContextMenu.tsx
@@ -6,7 +6,7 @@ export function ContextMenu({ children, title }: { children: ReactNode; title: s
   return (
     <Menu>
       <MenuButton>
-        <Icon as={BsThreeDots} color="gray.300" />
+        <Icon as={BsThreeDots} color="gray.300" mx={2} />
       </MenuButton>
       <MenuList>
         <MenuGroup title={title}>{children}</MenuGroup>
