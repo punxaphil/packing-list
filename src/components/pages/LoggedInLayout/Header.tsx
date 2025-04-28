bd2ea47379f1d1a58cb7ba78eff149fc1b71637d
Johan Frick <johan.frick@gmail.com>
2025-04-28 14:17:17 +0200

    feat: add button to switch lists

--- src/components/pages/LoggedInLayout/Header.tsx
+++ src/components/pages/LoggedInLayout/Header.tsx
@@ -1,5 +1,7 @@
-import { Flex, HStack, Input, Stack } from '@chakra-ui/react';
+import { Flex, HStack, IconButton, Input, Spacer, Stack } from '@chakra-ui/react';
 import { useMemo, useState } from 'react';
+import { GoArrowSwitch } from 'react-icons/go';
+import { useNavigate } from 'react-router';
 import { ProfileAvatar } from '~/components/auth/ProfileAvatar.tsx';
 import { usePackingList } from '~/providers/PackingListContext.ts';
 import { writeDb } from '~/services/database.ts';
@@ -8,6 +10,8 @@ import { NavButton } from './NavButton.tsx';
 export function Header() {
   const { packingList } = usePackingList();
   const [packingListName, setPackingListName] = useState('');
+  const navigate = useNavigate();
+
   useMemo(() => {
     setPackingListName(packingList.name);
   }, [packingList]);
@@ -22,21 +26,29 @@ export function Header() {
     <>
       <Flex align="center" justifyContent="space-between" m="3" key={packingList.id}>
         <img src="/squirrel_icon.png" alt="squirrel icon" />
+        <Spacer />
         <Input
           key={packingList.id}
           value={packingListName}
           onChange={(e) => savePackingListName(e.target.value)}
-          w="min"
+          w="max-content"
           textAlign="center"
           fontSize={['xl', '4xl']}
           p={0}
           variant="unstyled"
-          flexGrow={1}
           overflow="hidden"
           textOverflow="ellipsis"
           whiteSpace="nowrap"
-          mx="2"
         />
+        <IconButton
+          aria-label="Go to lists"
+          icon={<GoArrowSwitch />}
+          variant="ghost"
+          onClick={() => navigate('/packing-lists')}
+          size="sm"
+        />
+        <Spacer />
+
         <HStack w="50px" justifyContent="flex-end">
           <ProfileAvatar size="sm" />
         </HStack>
